bb48cb583a23db2b088dc759f2365c96b7ef3eca
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-03 08:37:30 -0500

    added some practice with variables

--- learning.py
+++ learning.py
@@ -13,3 +13,19 @@ print("Now I will count the eggs", 7)
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print(3 + 2 < 5 - 7)
 
+# Variables and some of their powers
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = space_in_a_car * cars_driven
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available today.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put approximately", average_passengers_per_car, "in each car.")
